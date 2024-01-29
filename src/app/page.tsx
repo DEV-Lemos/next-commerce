1a2961a317d3ee435add17e70509585fd8ae5e9f
DEV-Lemos <isac.lemos@outlook.com>
2024-01-29 12:37:03 -0300

    Listagem de produtos parte 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
 export default function Home() {
-    return <div>Next Commerce</div>;
+    return (
+        <div className="px-8 pt-8 mx-auto max-w-7xl xl:px-0 ">
+            <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:gap-6">
+                <div className="overflow-hidden bg-white rounded-lg shadow-lg">
+                    Prod 1
+                </div>
+                <div className="overflow-hidden bg-white rounded-lg shadow-lg">
+                    Prod 1
+                </div>
+                <div className="overflow-hidden bg-white rounded-lg shadow-lg">
+                    Prod 1
+                </div>
+                <div className="overflow-hidden bg-white rounded-lg shadow-lg">
+                    Prod 1
+                </div>
+                <div className="overflow-hidden bg-white rounded-lg shadow-lg">
+                    Prod 1
+                </div>
+            </div>
+        </div>
+    );
 }
